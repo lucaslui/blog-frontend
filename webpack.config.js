bb7f15bd92c714d2f602436747961fa47edf773d
Lucas Lui Motta <lucasluimotta@gmail.com>
2021-04-14 12:34:27 -0300

    feat: add externals and plugins, as well as fixing relative path problem of router

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
 const path = require('path')
-const { DefinePlugin } = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { DefinePlugin } = require('webpack')
 
 module.exports = {
   mode: 'development',
   entry: './src/main/index.tsx',
   output: {
-    path: path.join(__dirname, 'public/js'),
-    publicPath: '/public/js/',
-    filename: 'bundle.js'
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'main-bundle-[hash].js'
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', 'scss', 'gif', 'png'],
@@ -27,8 +30,11 @@ module.exports = {
         test: /\.scss$/,
         use: [
           {
-            loader: 'style-loader'
+            loader: MiniCssExtractPlugin.loader
           },
+          // {
+          //   loader: 'style-loader'
+          // },
           {
             loader: 'css-loader',
             options: {
@@ -72,9 +78,20 @@ module.exports = {
   },
   externals: {
     react: 'React',
-    'react-dom': 'ReactDOM'
+    'react-dom': 'ReactDOM',
+    'react-router-dom': 'ReactRouterDOM',
+    axios: 'axios'
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    }),
+    new FaviconsWebpackPlugin({
+      logo: './public/favicon.svg'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'main-bundle-[hash].css'
+    }),
     new CleanWebpackPlugin(),
     new DefinePlugin({
       'process.env.API_URL': JSON.stringify('https://espaco-de-conhecimento-backend.herokuapp.com/api')
