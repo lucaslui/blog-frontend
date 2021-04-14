5c7875ab8cd2e5f308d8a8a6f3bf40ad5079a9d6
Lucas Lui Motta <lucasluimotta@gmail.com>
2021-04-14 12:40:24 -0300

    feat: create logo component

--- src/presentation/components/logo/logo.tsx
+++ src/presentation/components/logo/logo.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import Styles from './logo-styles.scss'
+
 import LogoSVG from '../../assets/imgs/logo.svg'
 
 type Props = React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>
 
 const Logo: React.FC<Props> = (props: Props) => {
   return (
-    <img src={LogoSVG} width="200"/>
+    <div className={Styles.loginLogo}>
+      <img src={LogoSVG} alt="logo" />
+      <div className={Styles.label}>
+        <h1> Espaço de Conhecimento em IoT </h1>
+        <h2>
+          Tendo como objetivo o ensino de Internet das Coisas de forma
+          simples, prática e objetiva.
+        </h2>
+      </div>
+    </div>
   )
 }
 
