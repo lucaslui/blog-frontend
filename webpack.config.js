409841c28080c2e9986d93adf706cc1eb31bfae5
Lucas Lui Motta <lucasluimotta@gmail.com>
2021-04-18 01:34:56 -0300

    chore: add css test

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
         exclude: /node_modules/
       },
       {
-        test: /\.scss$/,
+        test: /\.(scss|css)$/,
         use: [
           {
             loader: MiniCssExtractPlugin.loader
